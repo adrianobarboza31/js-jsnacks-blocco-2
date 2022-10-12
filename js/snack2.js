2fb98d896fef7558639a97c86d566d305e8e39ed
adrianobarboza31 <barbozaa022@gmail.com>
2022-10-12 17:02:04 +0200

    add snack3

--- js/snack2.js
+++ js/snack2.js
@@ -1,3 +1,25 @@
 // Generatore di “nomi cognomi” casuali: il Grande Gatsby ha
 //   una lista di nomi e una lista di cognomi, e da queste vuole generare una
 //    falsa lista di invitati con nome e cognome.
+const listaNomi=["Adriano", "Federico","Andrea","Marco"];
+const listaCognome=["Barboza","Rossi","Bianchi","Rizzi"];
+
+let nomeRandom="";
+let cognomeRandom="";
+const bottone3 = document.getElementById("btn3")
+bottone3.addEventListener("click",function stampa2(){
+   for(i=0;i<listaNomi.length;i++){
+   iR=Math.floor(Math.random() * listaNomi.length);
+   nomeRandom=listaNomi[iR]
+   console.log(nomeRandom)
+   document.getElementById("nome").innerHTML+=nomeRandom+"<br>"
+   }
+
+   for(i=0;i<listaCognome.length;i++){
+    iR=Math.floor(Math.random() * listaCognome.length);
+    cognomeRandom=listaCognome[iR]
+    document.getElementById("cognome").innerHTML+=cognomeRandom+"<br>"
+    console.log(cognomeRandom)
+    }
+   
+})
--- js/snack3.js
+++ js/snack3.js
@@ -1,7 +1,7 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
 const lista =["1","2","3","4"];
 const bottone2 = document.getElementById("btn2")
-bottone2.addEventListener("click",function stampa1(){
+bottone2.addEventListener("click",function stampa3(){
     let somma=0
     for(i=0;i<lista.length;i++){
     somma +=parseInt(lista[i])
